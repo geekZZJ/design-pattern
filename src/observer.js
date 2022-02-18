e9a75e2bc83f182889e5e74a2da5ca636ea3304e
zzj <2461927976@qq.com>
2022-02-18 22:04:58 +0800

    观察者模式

---
+++ src/observer.js
@@ -0,0 +1,11 @@
+// 观察者模式
+function bindEvent(elem, type, selector, fn) {
+  if (fn === null) {
+    fn = selector;
+    selector = null;
+  }
+}
+
+// 调用
+bindEvent(elem, "click", "#div1", fn);
+bindEvent(elem, "click", fn);
