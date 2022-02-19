03d41f2f7ae5e8d5026cb53958faec9fe08c1cbe
zzj <2461927976@qq.com>
2022-02-19 20:49:02 +0800

    es6 for of已经实现了迭代器

--- src/iterator.js
+++ src/iterator.js
@@ -1,38 +1,65 @@
 // 迭代器模式
 
-class Iterator {
-  constructor(container) {
-    this.list = container.list;
-    this.index = 0;
-  }
+// class Iterator {
+//   constructor(container) {
+//     this.list = container.list;
+//     this.index = 0;
+//   }
+//
+//   next() {
+//     if (this.hasNext()) {
+//       return this.list[this.index++];
+//     }
+//     return null;
+//   }
+//
+//   hasNext() {
+//     return this.index < this.list.length;
+//
+//   }
+// }
+//
+// class Container {
+//   constructor(list) {
+//     this.list = list;
+//   }
+//
+//   // 生成遍历器
+//   getIterator() {
+//     return new Iterator(this);
+//   }
+// }
+//
+// const arr = [1, 2, 3, 4, 5, 6];
+// const container = new Container(arr);
+// const iterator = container.getIterator();
+// while (iterator.hasNext()) {
+//   console.log(iterator.next());
+// }
 
-  next() {
-    if (this.hasNext()) {
-      return this.list[this.index++];
-    }
-    return null;
-  }
-
-  hasNext() {
-    return this.index < this.list.length;
-
+// function each(data) {
+//   // 生成遍历器
+//   const iterator = data[Symbol.iterator]();
+//   let item = {done: false};
+//   while (!item.done) {
+//     item = iterator.next();
+//     if (!item.done) {
+//       console.log(item.value);
+//     }
+//   }
+// }
+function each(data){
+  for (const item of data) {
+    console.log(item)
   }
 }
 
-class Container {
-  constructor(list) {
-    this.list = list;
-  }
+const arr = [1, 2, 3, 4];
+const nodeList = document.getElementsByTagName("p");
+const m = new Map();
+m.set("a", 100);
+m.set("b", 200);
 
-  // 生成遍历器
-  getIterator() {
-    return new Iterator(this);
-  }
-}
-
-const arr = [1, 2, 3, 4, 5, 6];
-const container = new Container(arr);
-const iterator = container.getIterator();
-while (iterator.hasNext()) {
-  console.log(iterator.next());
-}
+each(arr);
+each(nodeList);
+each(m);
