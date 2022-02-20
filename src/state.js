1c364bc787c21da9beabfa7b1973c127c1d29e7c
zzj <2461927976@qq.com>
2022-02-20 11:38:26 +0800

    javascript-state-machine演示状态机

--- src/state.js
+++ src/state.js
@@ -1,5 +1,5 @@
 // 状态模式
-class State {
+/*class State {
   constructor(color) {
     this.color = color;
   }
@@ -39,4 +39,53 @@ console.log(context.getState())
 
 // 红灯亮了
 red.handle(context)
-console.log(context.getState())
+console.log(context.getState())*/
+
+// 初始化状态机模型
+import StateMachine from "javascript-state-machine";
+
+const fsm = new StateMachine({
+  init: "收藏",
+  transitions: [
+    {
+      name: "doStore",
+      from: "收藏",
+      to: "取消收藏"
+    },
+    {
+      name: "deleteStore",
+      from: "取消收藏",
+      to: "收藏"
+    }
+  ],
+  methods: {
+    // 监听执行收藏
+    onDoStore: function () {
+      alert("收藏成功");
+      updateText();
+    },
+    // 监听取消收藏
+    onDeleteStore: function () {
+      alert("已经取消收藏");
+      updateText();
+    }
+
+  }
+});
+
+const btn = document.getElementById("btn1");
+btn.onclick = function () {
+  if (fsm.is("收藏")) {
+    fsm.doStore();
+  } else {
+    fsm.deleteStore();
+  }
+};
+
+// 更新按钮的文案
+function updateText() {
+  btn.innerText = fsm.state;
+}
+
+// 初始化文案
+updateText();
