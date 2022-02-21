37a9fb1e023fdbd555d0aebeb044ae5517c5386b
zzj <2461927976@qq.com>
2022-02-21 23:46:41 +0800

    职责链模式

---
+++ src/cor.js
@@ -0,0 +1,24 @@
+class Action {
+  constructor(name) {
+    this.name = name;
+    this.nextAction = null;
+  }
+
+  setNextAction(action) {
+    this.nextAction = action;
+  }
+
+  handle() {
+    console.log(`${this.name} 审批`);
+    if (this.nextAction !== null) {
+      this.nextAction.handle();
+    }
+  }
+}
+
+const a1 = new Action("组长");
+const a2 = new Action("经理");
+const a3 = new Action("总监");
+a1.setNextAction(a2);
+a2.setNextAction(a3);
+a1.handle();
