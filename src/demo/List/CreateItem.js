7df1a9b8ec5db86255de22eba203400c97117b53
zzj <2461927976@qq.com>
2022-02-27 15:30:21 +0800

    折扣代理模式

--- src/demo/List/CreateItem.js
+++ src/demo/List/CreateItem.js
@@ -1,6 +1,24 @@
 import Item from "./Item";
 
+// 代理模式
+function createDiscount(itemData) {
+  return new Proxy(itemData, {
+    get(target, p, receiver) {
+      if (p === "name") {
+        return `${target[p]} 【折扣】`;
+      }
+      if (p === "price") {
+        return target[p] * 0.8;
+      }
+      return target[p];
+    }
+  });
+}
+
 // 工厂模式
 export default function (list, itemData) {
+  if (itemData.discount) {
+    itemData = createDiscount(itemData);
+  }
   return new Item(list, itemData);
 }
